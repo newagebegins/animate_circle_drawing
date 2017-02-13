2b4c6069a0b35b0bb7ec156824ea44bacc41c0c7
newagebegins <newagebegins@gmail.com>
2017-02-13 10:21:22 +1000

    back buffer

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 if not exist build mkdir build
 pushd build
-cl /nologo /Z7 /FC /W4 /WX ..\main.c /link /INCREMENTAL:NO user32.lib
+cl /nologo /Z7 /FC /W4 /WX ..\main.c /link /INCREMENTAL:NO user32.lib gdi32.lib
 popd
--- main.c
+++ main.c
@@ -1,6 +1,41 @@
 #include <windows.h>
 #include <stdbool.h>
 
+typedef enum {
+  COLOR_BLACK = 0x00000000
+} Color;
+
+typedef struct {
+  Color *memory;
+  size_t size;
+  int width;
+  int height;
+  BITMAPINFO info;
+} BackBuffer;
+
+BackBuffer makeBackBuffer(int width, int height) {
+  BackBuffer bb = {0};
+
+  bb.size = width * height * sizeof(*bb.memory);
+  bb.memory = malloc(bb.size);
+
+  bb.width = width;
+  bb.height = height;
+
+  bb.info.bmiHeader.biSize = sizeof(bb.info.bmiHeader);
+  bb.info.bmiHeader.biWidth = width;
+  bb.info.bmiHeader.biHeight = height;
+  bb.info.bmiHeader.biPlanes = 1;
+  bb.info.bmiHeader.biBitCount = 32;
+  bb.info.bmiHeader.biCompression = BI_RGB;
+
+  return bb;
+}
+
+void clearBackBuffer(BackBuffer *bb, Color color) {
+  memset(bb->memory, color, bb->size);
+}
+
 LRESULT CALLBACK wndProc(HWND wnd, UINT msg, WPARAM wparam, LPARAM lparam) {
   switch (msg) {
     case WM_DESTROY:
@@ -21,7 +56,7 @@ int CALLBACK WinMain(HINSTANCE inst, HINSTANCE prevInst, LPSTR cmdLine, int cmdS
   wndClass.lpfnWndProc = wndProc;
   wndClass.hInstance = inst;
   wndClass.hCursor = LoadCursor(0, IDC_ARROW);
-  wndClass.lpszClassName = "pinball";
+  wndClass.lpszClassName = "Pinball";
   RegisterClass(&wndClass);
 
   int windowWidth = 1920/2;
@@ -34,7 +69,7 @@ int CALLBACK WinMain(HINSTANCE inst, HINSTANCE prevInst, LPSTR cmdLine, int cmdS
   DWORD wndStyle = WS_OVERLAPPEDWINDOW | WS_VISIBLE;
   AdjustWindowRect(&crect, wndStyle, 0);
 
-  HWND wnd = CreateWindowEx(0, wndClass.lpszClassName, "pinball", wndStyle, 300, 0,
+  HWND wnd = CreateWindowEx(0, wndClass.lpszClassName, "Pinball", wndStyle, 300, 0,
                             crect.right - crect.left, crect.bottom - crect.top,
                             0, 0, inst, 0);
   ShowWindow(wnd, cmdShow);
@@ -47,6 +82,9 @@ int CALLBACK WinMain(HINSTANCE inst, HINSTANCE prevInst, LPSTR cmdLine, int cmdS
 
   QueryPerformanceFrequency(&perfcFreq);
   QueryPerformanceCounter(&perfc);
+
+  HDC hDC = GetDC(wnd);
+  BackBuffer bb = makeBackBuffer(windowWidth/2, windowHeight/2);
 
   bool running = true;
 
@@ -77,5 +115,10 @@ int CALLBACK WinMain(HINSTANCE inst, HINSTANCE prevInst, LPSTR cmdLine, int cmdS
           break;
       }
     }
+
+    clearBackBuffer(&bb, COLOR_BLACK);
+    StretchDIBits(hDC, 0, 0, windowWidth, windowHeight,
+                  0, 0, bb.width, bb.height, bb.memory,
+                  &bb.info, DIB_RGB_COLORS, SRCCOPY);
   }
 }
